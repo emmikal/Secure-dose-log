f964f0620360bd6aaa961ba7a59c1c9a565a604c
Emmi Kallioniemi <emmi.kallioniemi@protonmail.ch>
2026-08-01 13:03:42 +0300

    Bump version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.turboautismdoselog"
         minSdk = 30
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.0"
+        versionCode = 12
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
